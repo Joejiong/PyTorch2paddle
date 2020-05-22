e7f6e68c1b568bae1897289e95085b808a52bddd
wujionghao <wujionghao@baidu.com>
2020-05-22 17:04:59 +0800

    align paddle leNet with pytorch

--- Joe_nn_transfer/p2f_trans.py
+++ Joe_nn_transfer/p2f_trans.py
@@ -23,9 +23,10 @@ def check_for_missing_layers(paddle_layer_names, pytorch_layer_names, verbose):
         print("Layer names in paddle state_dict", paddle_layer_names)
 
     if not all(x in paddle_layer_names for x in pytorch_layer_names):
-        missing_layers = list(set(pytorch_layer_names) - set(paddle_layer_names))
-        raise Exception("Missing layer(s) in paddle HDF5 that are present" +
-                        " in state_dict: {}".format(missing_layers))
+        paddle_need_missing_layers = list(set(pytorch_layer_names) - set(paddle_layer_names))
+        paddle_extra_layers = list(set(paddle_layer_names) - set(pytorch_layer_names))
+        raise Exception("Missing layer(s) in paddle dictionary but not in pytorch layer(s) that are present" +
+                        " in state_dict: {0}, and paddle extra layers that are not in pytorch {1}".format(paddle_need_missing_layers, paddle_extra_layers))
 
 
 
@@ -34,13 +35,27 @@ def pytorch_to_paddle(pytorch_model, paddle_model,
 
 
     paddle_dict = paddle_model.state_dict()
-    fluid.save_dygraph(paddle_dict, "save_temp")
+    with fluid.dygraph.guard():
+        fluid.save_dygraph(paddle_dict, "save_temp")
+        
+    
+    model_w_dict = {}
+    for k, v in paddle_dict.items():
+        model_w_dict[k] = v
+
+
     pytorch_input_state_dict = pytorch_model.state_dict()
     pytorch_layer_names = util.state_dict_layer_names(pytorch_input_state_dict)
 
-    with open('save_temp', 'a') as f:
-        model_weights = f['model_weights']
-        target_layer_names = list(map(str, model_weights.keys()))
+    paddle_layer_names = util.state_dict_layer_names(paddle_dict)
+
+    with open('save_temp.pdparams', 'a') as f:
+        # model_weights = f['model_weights']
+        model_weights = model_w_dict
+
+        # target_layer_names = list(map(str, model_weights.keys()))
+        target_layer_names = paddle_layer_names
+
         check_for_missing_layers(
             target_layer_names,
             pytorch_layer_names,
@@ -48,12 +63,16 @@ def pytorch_to_paddle(pytorch_model, paddle_model,
 
         for layer in pytorch_layer_names:
 
-            paddle_h5_layer_param = util.dig_to_params_pf(model_weights[layer])
+            # paddle_h5_layer_param = util.dig_to_params_pf(model_weights[layer])
 
             weight_key = layer + '.weight'
             bias_key = layer + '.bias'
             running_mean_key = layer + '.running_mean'
             running_var_key = layer + '.running_var'
+
+            # paddle_h5_layer_param = util.dig_to_params_pf(model_weights[layer])
+            paddle_h5_layer_param = model_weights
+            print("niubi", f)
 
             # Load weights (or other learned parameters)
             if weight_key in pytorch_input_state_dict:
@@ -68,9 +87,7 @@ def pytorch_to_paddle(pytorch_model, paddle_model,
                 bias = pytorch_input_state_dict[bias_key].numpy()
                 if running_var_key in pytorch_input_state_dict:
                     paddle_h5_layer_param[bias_key][:] = bias
-                else:
-                    paddle_h5_layer_param[bias_key][:] = bias
-
+                
             # Load batch normalization running mean
             if running_mean_key in pytorch_input_state_dict:
                 running_mean = pytorch_input_state_dict[running_mean_key].numpy()
@@ -83,8 +100,10 @@ def pytorch_to_paddle(pytorch_model, paddle_model,
                 running_var += PYTORCH_EPSILON - PADDLE_EPSILON
                 paddle_h5_layer_param[PADDLE_MOVING_VARIANCE_KEY][:] = running_var
 
+
     # pytorch_model.load_state_dict(state_dict)
-    paddle_model.load_weights('temp.h5')
+    with fluid.dygraph.guard():
+        paddle_model.set_dict('save_temp.pdparams')
 
 
 def convert_weights(weights, to_pytorch=False, flip_filters=False, flip_channels=False):
--- Joe_nn_transfer/paddle_leNet.py
+++ Joe_nn_transfer/paddle_leNet.py
@@ -14,10 +14,11 @@ class LeNet(fluid.dygraph.Layer):
         self.conv2 = Conv2D(num_channels=6, num_filters=16, filter_size=5, act='relu')
         self.pool2 = Pool2D(pool_size=2, pool_stride=2, pool_type='max')
         
-        self.conv3 = Conv2D(num_channels=16, num_filters=120, filter_size=4, act='relu')
+        # self.conv3 = Conv2D(num_channels=16, num_filters=120, filter_size=4, act='relu')
         # 创建全连接层，第一个全连接层的输出神经元个数为64， 第二个全连接层输出神经元个数为分裂标签的类别数
-        self.fc1 = Linear(input_dim=120, output_dim=64, act='relu')
-        self.fc2 = Linear(input_dim=64, output_dim=num_classes)
+        self.fc1 = Linear(input_dim=16*5*5, output_dim=120, act='relu')
+        self.fc2 = Linear(input_dim=120, output_dim=84, act='relu')
+        self.fc3 = Linear(input_dim=84, output_dim=num_classes)
     # 网络的前向计算过程
     def forward(self, x):
         x = self.conv1(x)
